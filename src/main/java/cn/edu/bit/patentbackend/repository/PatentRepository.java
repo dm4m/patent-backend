41b31a0e7948a2258f5c159020d05917fb7c20e1
bwj <648507447@qq.com>
2022-02-04 16:21:29 +0800

    ip to config

--- src/main/java/cn/edu/bit/patentbackend/repository/PatentRepository.java
+++ src/main/java/cn/edu/bit/patentbackend/repository/PatentRepository.java
@@ -7,6 +7,7 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.search.SearchHits;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
@@ -14,10 +15,16 @@ import java.io.IOException;
 @Repository
 public class PatentRepository {
 
+    @Value("${elasticsearch.ip}")
+    private String es_ip;
+
+    @Value("${elasticsearch.port}")
+    private Integer es_port;
+
     public SearchHits search(SearchRequest request) throws IOException {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("localhost", 9200, "http")));
+                        new HttpHost(es_ip, es_port, "http")));
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
         // 查询匹配
         SearchHits hits = response.getHits();
