ba7c50cbdcc16193f685914481bdb18023ec71d6
byWhale <648507447@qq.com>
2022-07-18 21:56:26 +0800

    整合专业检索、简单检索、高级检索的代码，消除代码冗余

--- src/main/java/cn/edu/bit/patentbackend/service/PatentServiceImpl.java
+++ src/main/java/cn/edu/bit/patentbackend/service/PatentServiceImpl.java
@@ -34,65 +34,27 @@ public class PatentServiceImpl implements PatentService{
 
     @Override
     public BasicSearchResponse basicSearch(String query, String field, Integer curPage, Integer perPage) throws IOException {
-        curPage = curPage >= 0 ? curPage : 0;
-        perPage = perPage > 0 ? perPage : 20;
-        int from = curPage * perPage;
-        int size = (perPage);
-        ArrayList<Map<String, Object>> results = new ArrayList<>();
-        // 创建搜索请求对象
-        SearchRequest request = new SearchRequest();
-        request.indices("patent");
-        // 构建查询的请求体
-        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        QueryBuilder queryBuilder = null;
         if(field.equals("title")){
-            sourceBuilder.query(QueryBuilders.matchQuery("title", query));
+            queryBuilder = QueryBuilders.matchQuery("title", query);
         }else if(field.equals("abstract")){
-            sourceBuilder.query(QueryBuilders.matchQuery("abstract", query));
-        }else if(field.equals("signory_item")){
-            sourceBuilder.query(QueryBuilders.matchQuery("signory_item", query));
+            queryBuilder = QueryBuilders.matchQuery("abstract", query);
+        }else if(field.equals("signoryItem")){
+            queryBuilder = QueryBuilders.matchQuery("signory_item", query);
         }else if(field.equals("patentCode")){
-            sourceBuilder.query(QueryBuilders.termQuery("patent_code.keyword", query));
+            queryBuilder = QueryBuilders.termQuery("patent_code.keyword", query);
         }else if(field.equals("publicationNo")){
-            sourceBuilder.query(QueryBuilders.termQuery("publication_no.keyword", query));
+            queryBuilder = QueryBuilders.termQuery("publication_no.keyword", query);
         }else if(field.equals("applicant")){
-            sourceBuilder.query(QueryBuilders.wildcardQuery("applicant_list.keyword", "*" + query + "*"));
+            queryBuilder = QueryBuilders.wildcardQuery("applicant_list.keyword", "*" + query + "*");
         }else if(field.equals("inventor")){
-            sourceBuilder.query(QueryBuilders.wildcardQuery("inventor_list.keyword", "*" + query + "*"));
-        }else if(field.equals("main_class")){
-            sourceBuilder.query(QueryBuilders.wildcardQuery("main_class_list.keyword", "*" + query + "*"));
+            queryBuilder = QueryBuilders.wildcardQuery("inventor_list.keyword", "*" + query + "*");
+        }else if(field.equals("mainClass")){
+            queryBuilder = QueryBuilders.wildcardQuery("main_class_list.keyword", "*" + query + "*");
         }else if(field.equals("class")){
-            sourceBuilder.query(QueryBuilders.wildcardQuery("class_list.keyword", "*" + query + "*"));
+            queryBuilder = QueryBuilders.wildcardQuery("class_list.keyword", "*" + query + "*");
         }
-//        sourceBuilder.query(QueryBuilders.multiMatchQuery(query, field));
-        sourceBuilder.from(from);
-        sourceBuilder.size(size);
-        request.source(sourceBuilder);
-        SearchHits hits = null;
-        try {
-            hits = patentRepository.basicSearch(request);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        }
-        System.out.println("total:" + hits.getTotalHits());
-        System.out.println("MaxScore:" + hits.getMaxScore());
-        System.out.println("hits========>>");
-        int i = 0;
-        for (SearchHit hit : hits) {
-            //输出每条查询的结果信息
-            String sourceAsString = hit.getSourceAsString();
-            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
-            //生成序号
-            sourceAsMap.put("index", curPage * perPage + (++i));
-            results.add(sourceAsMap);
-            System.out.println(hit.getSourceAsString());
-        }
-        System.out.println("<<========");
-        System.out.println("收到请求");
-        long totalHits = hits.getTotalHits().value;
-        int pageNum = (int)totalHits / perPage;
-        BasicSearchResponse response = new BasicSearchResponse(curPage, totalHits, pageNum, perPage, query, field,"basic", results);
+        BasicSearchResponse response = patentRepository.searchByQueryBuilder(queryBuilder, curPage, perPage);
         return response;
     }
 
@@ -124,46 +86,8 @@ public class PatentServiceImpl implements PatentService{
 
     @Override
     public BasicSearchResponse proSearch(String expression, Integer curPage, Integer perPage) {
-        curPage = curPage >= 0 ? curPage : 0;
-        perPage = perPage > 0 ? perPage : 20;
-        int from = curPage * perPage;
-        int size = (perPage);
-        ArrayList<Map<String, Object>> results = new ArrayList<>();
         QueryBuilder queryBuilder = ExpressionUtil.expression2Query(expression);
-
-        System.out.println(queryBuilder);
-
-        SearchRequest searchRequest = new SearchRequest();
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.query(queryBuilder);
-        searchSourceBuilder.from(from);
-        searchSourceBuilder.size(size);
-        searchRequest.source(searchSourceBuilder);
-        SearchHits hits = null;
-        try {
-            hits = patentRepository.basicSearch(searchRequest);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        } catch (KeyManagementException e) {
-            throw new RuntimeException(e);
-        }
-        int i = 0;
-        for (SearchHit hit : hits) {
-            //输出每条查询的结果信息
-            String sourceAsString = hit.getSourceAsString();
-            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
-            //生成序号
-            sourceAsMap.put("index", curPage * perPage + (++i));
-            results.add(sourceAsMap);
-            System.out.println(hit.getSourceAsString());
-        }
-        System.out.println("<<========");
-        System.out.println("收到请求");
-        long totalHits = hits.getTotalHits().value;
-        int pageNum = (int)totalHits / perPage;
-        BasicSearchResponse response = new BasicSearchResponse(curPage, totalHits, pageNum, perPage, expression, "","pro", results);
+        BasicSearchResponse response = patentRepository.searchByQueryBuilder(queryBuilder, curPage, perPage);
         return response;
     }
 
